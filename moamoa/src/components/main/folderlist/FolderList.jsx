3f92235b1cfe11fee59daefe7a267fdb3096f927
Suki <tues1205@naver.com>
2025-05-17 02:54:01 +0900

    ✨ : 새폴더 생성 기능

--- moamoa/src/components/main/folderlist/FolderList.jsx
+++ moamoa/src/components/main/folderlist/FolderList.jsx
@@ -4,19 +4,29 @@ import { FaPlus } from "react-icons/fa6";
 import FolderListItem from "./FolderListItem";
 
 const FolderList = () => {
-  const [editingId, setEditingId] = useState(null); // 수정 모드인 폴더
-
-  const folders = [
+  const [folders, setFolders] = useState([
     { id: 1, name: "폴더1" },
     { id: 2, name: "폴더2" },
     { id: 3, name: "폴더3" },
-  ];
+  ]);
+
+  const [editingId, setEditingId] = useState(null); // 수정 모드인 폴더
+
+  // 새폴더 추가
+  const handleAddFolder = () => {
+    const newId =
+      folders.length > 0 ? Math.max(...folders.map((f) => f.id)) + 1 : 1;
+    const newFolder = { id: newId, name: "새 폴더" };
+
+    setFolders([...folders, newFolder]);
+    setEditingId(newId);
+  };
 
   return (
     <div className="folder-list">
       <div className="folder-list__header">
         <span className="folder-list__title">폴더</span>
-        <FaPlus className="folder-list__add-button" />
+        <FaPlus onClick={handleAddFolder} className="folder-list__add-button" />
       </div>
 
       <div className="folder-list__items">
