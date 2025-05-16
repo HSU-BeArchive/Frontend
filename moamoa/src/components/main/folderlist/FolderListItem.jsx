8dbbb84a2cb32d3da0658649452a643801a0f8be
Suki <tues1205@naver.com>
2025-05-17 02:33:06 +0900

    ✨ : FolderListItem 목록

---
+++ moamoa/src/components/main/folderlist/FolderListItem.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { FaEdit, FaCheck, FaTimes } from "react-icons/fa";
+import "./FolderList.scss";
+import { GoPencil } from "react-icons/go";
+import { IoCheckmark, IoCloseOutline } from "react-icons/io5";
+
+const FolderListItem = ({
+  /*id,*/ name,
+  isEditing,
+  onStartEdit,
+  onStopEdit,
+}) => {
+  const [inputValue, setInputValue] = useState(name); // 입력 이름
+  const [originalValue, setOriginalValue] = useState(name); // 원래 이름
+
+  const isModified = inputValue !== originalValue; // 수정 여부
+
+  // 이름 수정
+  const handleSave = () => {
+    setOriginalValue(inputValue); // 저장된 이름 갱신
+    onStopEdit();
+  };
+
+  // 이름 수정 취소
+  const handleCancel = () => {
+    setInputValue(originalValue); // 원래 이름으로
+    onStopEdit();
+  };
+
+  return (
+    <div className={`folder-item ${isEditing ? "folder-item--editing" : ""}`}>
+      {isEditing ? (
+        <div className="folder-item__edit-wrapper">
+          <input
+            className="folder-item__input"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <div className="folder-item__icons">
+            <IoCheckmark
+              className={`folder-item__icon folder-item__icon--confirm ${
+                isModified ? "active" : ""
+              }`}
+              onClick={handleSave}
+            />
+            <IoCloseOutline
+              className="folder-item__icon folder-item__icon--cancel"
+              onClick={handleCancel}
+            />
+          </div>
+        </div>
+      ) : (
+        <>
+          <span className="folder-item__name">{originalValue}</span>
+          <GoPencil className="folder-item__icon" onClick={onStartEdit} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default FolderListItem;
