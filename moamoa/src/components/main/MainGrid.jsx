5245cbb0f2309023f73c053baf67ac49968e77cf
Suki <tues1205@naver.com>
2025-05-18 02:52:14 +0900

    ✨ : MainGrid 컴포넌트 뼈대

---
+++ moamoa/src/components/main/MainGrid.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./MainGrid.scss";
+import dummyFolders from "../../data/dummyFolders.json";
+import FolderItem from "./folderitem/FolderItem";
+
+const MainGrid = () => {
+  return (
+    <div className="main-grid">
+      <div className="folder-grid">
+        <div className="folder-grid__header">
+          <span className="folder-grid__title">아카이브폴더</span>
+        </div>
+        <div className="folder-grid__items">
+          {dummyFolders.map((folder) => (
+            <FolderItem
+              key={folder.id}
+              name={folder.name}
+              isEmpty={folder.isEmpty}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainGrid;
