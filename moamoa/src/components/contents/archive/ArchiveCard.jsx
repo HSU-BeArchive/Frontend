7748c126549c4952c0554b279747eaef165393e9
yj-044 <yunji8533@naver.com>
2025-05-20 19:33:42 +0900

    💄: 레퍼런스 삭제 버튼 아이콘 스타일로 변경

--- moamoa/src/components/contents/archive/ArchiveCard.jsx
+++ moamoa/src/components/contents/archive/ArchiveCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { RxCross2 } from "react-icons/rx";
 import "./Archive.scss";
 
 const ArchiveCard = ({ name, onDelete }) => {
@@ -6,7 +7,7 @@ const ArchiveCard = ({ name, onDelete }) => {
     <div className="archive-card">
       <div className="card-thumbnail">
         <button className="delete-button" onClick={onDelete}>
-          ×
+          <RxCross2 size="100%" />
         </button>
       </div>
       <div className="card-name">{name}</div>
