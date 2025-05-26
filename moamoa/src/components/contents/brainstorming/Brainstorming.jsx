f7c85f3bf375f88a29443dac3196eed22e746e04
Suki <tues1205@naver.com>
2025-05-26 18:49:47 +0900

    ✨ : 레퍼런스 단일 조회 api 연동

--- moamoa/src/components/contents/brainstorming/Brainstorming.jsx
+++ moamoa/src/components/contents/brainstorming/Brainstorming.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import getOneRefApi from "../../../api/brainstorming/getOneRefApi";
 import ReferenceInfo from "./ReferenceInfo";
 import ChatArea from "./ChatArea";
-import useRefData from "../../../hooks/useRefData";
 import "./Brainstorming.scss";
 
 const Brainstorming = () => {
   const { folderId, refId } = useParams();
-  const referenceData = useRefData(); // 레퍼런스 데이터
+  const [referenceData, setReferenceData] = useState(null); // 레퍼런스 데이터
+
+  useEffect(() => {
+    const fetchReference = async () => {
+      // 레퍼런스 단일 조회 api 호출
+      const data = await getOneRefApi(folderId, refId);
+      setReferenceData(data);
+    };
+    fetchReference();
+  }, [folderId, refId]);
 
   return (
     <div className="brainstorming">
@@ -15,7 +24,6 @@ const Brainstorming = () => {
         <span className="brainstorming__title">아카이브 보드</span>
       </div>
       <div className="brainstorming__chat">
-        {/* folderId, refId 전달 예정 */}
         <ReferenceInfo data={referenceData} folderId={folderId} refId={refId} />
         <ChatArea />
       </div>
